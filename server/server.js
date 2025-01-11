fa603ed50b0bf99dd3b68f4687934eb5fade0961
rafsan3051 <rafsan3051@gmail.com>
2025-01-11 15:01:29 +0600

    9.12.50

--- server/server.js
+++ server/server.js
@@ -48,7 +48,8 @@ app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/admin/products", adminProductsRouter);
 app.use("/api/shop/products", shopProductsRouter);
-
+app.use("/api/shop/cart", shopCartRouter);
+app.use("/api/shop/cart", shopAddressRouter);
 
 app.listen(PORT, () => 
     console.log(`Server is running on port ${PORT}`))
