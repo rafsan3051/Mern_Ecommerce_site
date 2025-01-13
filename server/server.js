dc71887209d112c5dbdbc717582685226d9d60fe
rafsan3051 <rafsan3051@gmail.com>
2025-01-13 14:06:44 +0600

    13.10.01

--- server/server.js
+++ server/server.js
@@ -47,10 +47,12 @@ app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/admin/products", adminProductsRouter);
+app.use("/api/admin/orders", adminOrderRouter);
 app.use("/api/shop/products", shopProductsRouter);
 app.use("/api/shop/cart", shopCartRouter);
 app.use("/api/shop/address", shopAddressRouter);
 app.use("/api/shop/order", shopOrderRouter);
+app.use("/api/shop/search", shopSearchRouter)
 
 app.listen(PORT, () => 
     console.log(`Server is running on port ${PORT}`))
